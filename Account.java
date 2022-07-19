bf0e7520034ed098f7210f027f08d4dfa60c9b4b
Gift   Laizer <69000738+Gymo1590@users.noreply.github.com>
2022-07-19 11:40:58 +0300

    Update Account.java

--- Account.java
+++ Account.java
@@ -4,7 +4,7 @@ import java.security.KeyPair;
 
 public class Account{
 
-  int balance =1;
+  private int balance =1;
   String ID;
   KeyPair voteWallet;
   // in my case ,i don't really need a list of wallet address , i only need one because u can only vote once ,only one transaction that sends one vote to the candidate
