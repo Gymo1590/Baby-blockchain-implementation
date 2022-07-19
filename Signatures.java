b7bf0e56a26e5345c48c05c8f8f5846fbc19cd65
Gift   Laizer <69000738+Gymo1590@users.noreply.github.com>
2022-07-20 01:36:14 +0300

    Update Signatures.java

--- Signatures.java
+++ Signatures.java
@@ -1,25 +1,30 @@
-
+package babyblockchain;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
 public class Signatures {
-    private static final String
-        ALGO
-        = "SHA256withRSA";
+   
 
-     static   keyPairs gymo = new keyPairs();
-    public static byte[] GenerateSignature(
+     
+    public  byte[] GenerateSignature(
         byte[] input,
        PrivateKey key )
-        throws Exception
+        
     {
-        key =gymo.keys().getPrivate();
-        Signature signature
-            = Signature.getInstance(
-                ALGO); 
-        signature.initSign( key);
-        signature.update(input);
-        return signature.sign();
+    	byte[] sigval = new byte[0];
+        Signature signature;
+		try {
+			signature = Signature.getInstance(
+            		"SHA256withRSA");
+			  signature.initSign( key);
+		        signature.update(input);
+		        sigval=    signature.sign();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+		} 
+		return sigval;
+      
     }
   
   
@@ -29,16 +34,22 @@ public class Signatures {
         byte[] input,
         byte[] signatureToVerify,
        PublicKey key)
-        throws Exception
+        
     {
-        key = gymo.keys().getPublic();
-        Signature signature
-            = Signature.getInstance(
-               ALGO);
-        signature.initVerify( key);
-        signature.update(input);
-        return signature
-            .verify(signatureToVerify);
+      
+        Signature signature;
+		try {
+			signature = Signature.getInstance(
+            		"SHA256withRSA");
+			   signature.initVerify( key);
+		        signature.update(input);
+		    return signature
+		            .verify(signatureToVerify);
+		} catch ( Exception e) {
+			throw new RuntimeException(e);
+		}
+
+     
     }
    
 }
