9c14e3171d198f2ce71b1c25355d2f7a3d1a254f
Gift   Laizer <69000738+Gymo1590@users.noreply.github.com>
2022-07-25 02:08:34 +0300

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,45 +1,31 @@
-package babyblockchain;
-
+ 
 import java.security.KeyPair;
 import java.security.PrivateKey;
-import java.security.PublicKey;
-
 public class Account{
 
- private int balance =1;
-  String ID ;
-  KeyPair voteWallet;
-  // in my case ,i don't really need a list of wallet address , i only need one because u can only vote once ,only one transaction that sends one vote to the candidate
-  keyPairs template = new keyPairs();
- KeyPair getAccount(){
+ private int vote;
 
-  try {
-    voteWallet = template.keys();
+  KeyPair voteWallet= keyPairs.keys() ;
+   String ID = calculateHash();
  
-  } catch (Exception e) {
-  System.out.println("Unknown error has ccured");
-  }
-  return voteWallet;
- }
-
- public int updateSendBalance(int balance) {
-    return this.balance - balance;
- }
- public int updateReceiveBalance(int balance) {
-  return this.balance + balance;
+ public void updateBalance(int balance) {
+ this.vote =balance;
 }
 public int getBalance() {
-    return balance;
-}
-public byte[] signData()  {
-	ID=Hash.hash_sha256(getAccount().getPublic().toString()+Integer.toString(balance));
-	  Signatures tempo = new Signatures();
-	return   tempo.GenerateSignature( ID.getBytes(), getAccount().getPrivate());
-	
-	
-	
-	
+    return vote;
 }
 
+public static byte[] signData(PrivateKey key, String msg)  {
+	
+	return   Signatures.GenerateSignature( msg.getBytes(), key);
+
+}
+private  String calculateHash() {
+	 
+	return Hash.hash_sha256(
+			voteWallet.getPublic().toString()
+				);
+	  
+}
  
 }
