e8fac79371ade443c00edebab8f113ebae9a10cd
Gift   Laizer <69000738+Gymo1590@users.noreply.github.com>
2022-07-29 01:28:34 +0300

    Update Block.java

--- Block.java
+++ Block.java
@@ -11,24 +11,28 @@ public class Block {
 	public ArrayList<Transaction> transactions = new ArrayList<Transaction>();
  
 	public int nonce;
-	public  Block( String previousHash){
-
+	public  Block( String previousHash ){
+       
 		this.prevHash=previousHash;
 	};	 
-	public Block createBlock( ) {
-		Block newBlock = new Block(prevHash);
+	public Block createBlock(   ) {
+		Block newBlock = new Block(prevHash );
 	 if (blockVerify() == false){
+		System.out.println("failed block verification");
+        return null;
+		
 	 }else{
 		
-		newBlock.transactions = transactions;
 		newBlock.hashOfBlock = blockHash();
 		newBlock.prevHash = prevHash;
 
 	 }
-		 
+		//  System.out.println("check block cretaio" +newBlock==null);
 	 return  newBlock;
 
 	 }
+
+
 	 //this will verify  the block
      public Boolean blockVerify(){
     
@@ -40,20 +44,9 @@ public class Block {
 			 System.out.println("cant add an empty transaction");
 			 return false;
 			 } 
-			   else if( this.transactions.get(j).createTransaction()==null){
-					System.out.println("empty transaction cant be added to the block!");
-				return false;
-	   
-				 } 
-				 else if(this.transactions.get(j).operation.sender.getBalance() ==0){
-				   // sender can only have null or 1 vote value .if its zero means he/she already voted so the vote value was set to 0
-				   return false;
-				 }
-				 else if(this.transactions.get(j).ID == this.transactions.get(j-1).ID){
-	
-				 System.out.println("Conflicting transactions!!");
-				 return false;
-				 }
+			  
+			 
+				 
 			 }
 		return true;
 	   }
