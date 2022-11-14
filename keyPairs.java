bdb97bdc2701d1efba701e8a7ff6f09c8d84ed43
Gift   Laizer <69000738+Gymo1590@users.noreply.github.com>
2022-11-14 19:28:47 +0300

    Update keyPairs.java

--- keyPairs.java
+++ keyPairs.java
@@ -1,5 +1,6 @@
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
@@ -7,12 +8,19 @@ import java.security.SecureRandom;
 public class keyPairs{
    static  PublicKey publicKey ;
  static   PrivateKey privKey ;
-   public KeyPair keys() throws Exception{
+   public static KeyPair keys()  {
     SecureRandom secureRandom
     = new SecureRandom();
-      KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
-      keyPairGen.initialize(2048, secureRandom);  
-      KeyPair pair = keyPairGen.generateKeyPair();
+      KeyPairGenerator keyPairGen;
+      KeyPair pair = null;
+      try {
+        keyPairGen = KeyPairGenerator.getInstance("RSA");
+        keyPairGen.initialize(2048, secureRandom); 
+         pair = keyPairGen.generateKeyPair();
+      } catch (NoSuchAlgorithmException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
      return pair;
    }
   
